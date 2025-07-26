9078c22be55cec5e42b44f968838964e9240ba9c
김형관 <bearson0115@gmail.com>
2025-07-26 13:33:59 +0900

    firstCommit 2025_07_26_1333

---
+++ src/main/java/com/secondprojinitiumback/SecondProjInitiumBackApplication.java
@@ -0,0 +1,13 @@
+package com.secondprojinitiumback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondProjInitiumBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondProjInitiumBackApplication.class, args);
+    }
+
+}

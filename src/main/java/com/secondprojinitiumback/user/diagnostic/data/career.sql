16903037a48838af1a3b1eacd5c280e3ecc62ea8
shin inho <strongbox1@naver.com>
2025-07-28 21:19:10 +0900

    7/28 데이터 추가

---
+++ src/main/java/com/secondprojinitiumback/user/diagnostic/data/career.sql
@@ -0,0 +1,14 @@
+-- 직업가치관검사 - 대학생
+INSERT INTO EX_DGNSTC_TST(EX_DGNSTC_TST_NM, PRVD_NM, QESTRN_SEQ_NO, TRGET_SE_CD, DGNSTC_TST_DC) VALUES('직업가치관검사 - 대학생', '커리어넷', '6', '100208', '대학생 대상 직업가치관 검사');
+
+-- 직업가치관검사 - 일반
+INSERT INTO EX_DGNSTC_TST(EX_DGNSTC_TST_NM, PRVD_NM, QESTRN_SEQ_NO, TRGET_SE_CD, DGNSTC_TST_DC) VALUES('직업가치관검사 - 일반', '커리어넷', '6', '100209', '일반 대상 직업가치관 검사');
+
+-- 주요능력효능감검사
+INSERT INTO EX_DGNSTC_TST(EX_DGNSTC_TST_NM, PRVD_NM, QESTRN_SEQ_NO, TRGET_SE_CD, DGNSTC_TST_DC) VALUES('주요능력효능감검사', '커리어넷', '10', '100208', '대학생 대상 주요 능력 효능감 측정 검사');
+
+-- 진로개발준비도검사
+INSERT INTO EX_DGNSTC_TST(EX_DGNSTC_TST_NM, PRVD_NM, QESTRN_SEQ_NO, TRGET_SE_CD, DGNSTC_TST_DC) VALUES('진로개발준비도검사', '커리어넷', '8', '100208', '대학생 대상 진로개발 준비도 검사');
+
+-- 이공계전공적합도검사
+INSERT INTO EX_DGNSTC_TST(EX_DGNSTC_TST_NM, PRVD_NM, QESTRN_SEQ_NO, TRGET_SE_CD, DGNSTC_TST_DC) VALUES('이공계전공적합도검사', '커리어넷', '9', '100208', '대학생 대상 이공계열 전공 적합도 검사');

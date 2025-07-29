ed935268d85de6e543513d913756e4d6e52ceba7
김형관 <bearson0115@gmail.com>
2025-07-29 17:45:59 +0900

    2025_07_29_1745

---
+++ src/main/java/com/secondprojinitiumback/user/student/dto/addStudentDto.java
@@ -0,0 +1,14 @@
+package com.secondprojinitiumback.user.student.dto;
+
+import java.time.LocalDate;
+
+public class addStudentDto {
+
+    private String name;
+    private String SchoolSubjectCode;
+    private String gender;
+    private String email;
+    private String bankAccountNumber;
+    private LocalDate birthDate;
+    private LocalDate admissionDate;
+}
---
+++ src/main/java/com/secondprojinitiumback/user/student/service/serviceinterface/StudentService.java
@@ -0,0 +1,6 @@
+package com.secondprojinitiumback.user.student.service.serviceinterface;
+
+public interface StudentService {
+
+    String addStudent();
+}

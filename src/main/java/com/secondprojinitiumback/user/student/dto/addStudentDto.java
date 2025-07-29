98c8ba67c85cb2b84abb169d7c30f990180df8ff
김형관 <bearson0115@gmail.com>
2025-07-29 17:50:23 +0900

    2025_07_29_1750

--- src/main/java/com/secondprojinitiumback/user/student/dto/addStudentDto.java
+++ src/main/java/com/secondprojinitiumback/user/student/dto/addStudentDto.java
@@ -1,12 +1,14 @@
 package com.secondprojinitiumback.user.student.dto;
 
+import com.secondprojinitiumback.common.constant.Gender;
+
 import java.time.LocalDate;
 
 public class addStudentDto {
 
     private String name;
     private String SchoolSubjectCode;
-    private String gender;
+    private Gender gender;
     private String email;
     private String bankAccountNumber;
     private LocalDate birthDate;

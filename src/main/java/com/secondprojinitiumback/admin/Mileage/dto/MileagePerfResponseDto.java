43eaabbf6574d40184ccc5572c28cb2e1956f083
jungyouri <jyouri0306@naver.com>
2025-08-04 03:50:46 +0900

    25-08-04
    # Conflicts:
    #	src/main/java/com/secondprojinitiumback/admin/Mileage/domain/MileagePerf.java

--- src/main/java/com/secondprojinitiumback/admin/Mileage/dto/MileagePerfResponseDto.java
+++ src/main/java/com/secondprojinitiumback/admin/Mileage/dto/MileagePerfResponseDto.java
@@ -10,8 +10,12 @@ import java.time.LocalDateTime;
 @Builder
 public class MileagePerfResponseDto {
 
-    private Long id;                   // 실적 ID
-    private String studentNo;         // 학번
+    private Long id;                  // 실적 ID
+
+    private String studentNo;        // 학번
+    private String name;      // 이름
+    private String schoolSubjectName;   // 학과명
+
     private Integer accMlg;           // 적립 마일리지
     private LocalDateTime createdAt;  // 등록일
     private LocalDateTime canceledAt; // 취소일 (nullable)
@@ -19,6 +23,7 @@ public class MileagePerfResponseDto {
 
     private Long mileageItemId;       // 마일리지 항목 ID
     private String mileageItemCode;   // 마일리지 항목 코드
+    private String eduNm; // 비교과 프로그램명
 
     private Long scorePolicyId;       // 배점 정책 ID
     private String scoreCriteria;     // 배점 조건
@@ -29,15 +34,23 @@ public class MileagePerfResponseDto {
     public static MileagePerfResponseDto from(MileagePerf perf) {
         return MileagePerfResponseDto.builder()
                 .id(perf.getId())
-                .studentNo(perf.getStudentNo())
+
+                .studentNo(perf.getStudent().getStudentNo())
+                .name(perf.getStudent().getName())
+                .schoolSubjectName(perf.getStudent().getSchoolSubject().getSubjectName())
+
                 .accMlg(perf.getAccMlg())
                 .createdAt(perf.getCreatedAt())
                 .canceledAt(perf.getCanceledAt())
                 .cancelReason(perf.getCancelReason())
+
                 .mileageItemId(perf.getMileageItem().getId())
                 .mileageItemCode(perf.getMileageItem().getItemCode())
+                .eduNm(perf.getMileageItem().getProgram().getEduNm())
+
                 .scorePolicyId(perf.getScorePolicy().getId())
                 .scoreCriteria(perf.getScorePolicy().getScoreCriteria())
+
                 //.scholarshipApplyId(perf.getScholarshipApply() != null ? perf.getScholarshipApply().getId() : null)
                 .build();
     }

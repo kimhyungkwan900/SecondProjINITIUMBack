3f9762c019e096ab8c190675bec674a7cf7b8623
kim Gun ho <vlfh0002@gmail.com>
2025-08-13 16:26:30 +0900

    0813

---
+++ src/main/java/com/secondprojinitiumback/admin/extracurricular/data/ExtracurricularSchedule.sql
@@ -0,0 +1,63 @@
+-- 1. 기획 역량 강화 워크숍 A (2025-09-10 ~ 2025-09-12)
+INSERT INTO EXTRACURRICULAR_SCHEDULE (EDU_MNG_ID, EDU_DT, EDU_EDN_TM) VALUES
+                                                                          (1, '2025-09-10 09:00:00', '17:00:00'),
+                                                                          (1, '2025-09-12 09:00:00', '17:00:00');
+
+-- 2. 기획 역량 강화 워크숍 B (2025-09-20 ~ 2025-09-22)
+INSERT INTO EXTRACURRICULAR_SCHEDULE (EDU_MNG_ID, EDU_DT, EDU_EDN_TM) VALUES
+                                                                          (2, '2025-09-20 09:00:00', '17:00:00'),
+                                                                          (2, '2025-09-22 09:00:00', '17:00:00');
+
+-- 3. 전략 수립 세미나 A (2025-09-25 ~ 2025-09-25)
+INSERT INTO EXTRACURRICULAR_SCHEDULE (EDU_MNG_ID, EDU_DT, EDU_EDN_TM) VALUES
+    (3, '2025-09-25 09:00:00', '17:00:00');
+
+-- 4. 전략 실행 워크숍 B (2025-10-01 ~ 2025-10-03)
+INSERT INTO EXTRACURRICULAR_SCHEDULE (EDU_MNG_ID, EDU_DT, EDU_EDN_TM) VALUES
+                                                                          (4, '2025-10-01 09:00:00', '17:00:00'),
+                                                                          (4, '2025-10-03 09:00:00', '17:00:00');
+
+-- 5. 전략 컨퍼런스 2025 (2025-10-05 ~ 2025-10-05)
+INSERT INTO EXTRACURRICULAR_SCHEDULE (EDU_MNG_ID, EDU_DT, EDU_EDN_TM) VALUES
+    (5, '2025-10-05 09:00:00', '17:00:00');
+
+-- 6. 전략 기획 워크숍 (2025-10-08 ~ 2025-10-10)
+INSERT INTO EXTRACURRICULAR_SCHEDULE (EDU_MNG_ID, EDU_DT, EDU_EDN_TM) VALUES
+                                                                          (6, '2025-10-08 09:00:00', '17:00:00'),
+                                                                          (6, '2025-10-10 09:00:00', '17:00:00');
+
+-- 7. 리더십 기본 과정 A (2025-10-15 ~ 2025-10-15)
+INSERT INTO EXTRACURRICULAR_SCHEDULE (EDU_MNG_ID, EDU_DT, EDU_EDN_TM) VALUES
+    (7, '2025-10-15 09:00:00', '17:00:00');
+
+-- 8. 리더십 심화 워크숍 B (2025-10-20 ~ 2025-10-22)
+INSERT INTO EXTRACURRICULAR_SCHEDULE (EDU_MNG_ID, EDU_DT, EDU_EDN_TM) VALUES
+                                                                          (8, '2025-10-20 09:00:00', '17:00:00'),
+                                                                          (8, '2025-10-22 09:00:00', '17:00:00');
+
+-- 9. 관리 스킬 향상 세미나 A (2025-10-25 ~ 2025-10-25)
+INSERT INTO EXTRACURRICULAR_SCHEDULE (EDU_MNG_ID, EDU_DT, EDU_EDN_TM) VALUES
+    (9, '2025-10-25 09:00:00', '17:00:00');
+
+-- 10. 관리 전략 워크숍 B (2025-11-01 ~ 2025-11-03)
+INSERT INTO EXTRACURRICULAR_SCHEDULE (EDU_MNG_ID, EDU_DT, EDU_EDN_TM) VALUES
+                                                                          (10, '2025-11-01 09:00:00', '17:00:00'),
+                                                                          (10, '2025-11-03 09:00:00', '17:00:00');
+
+-- 11. 성과 관리 특강 A (2025-11-10 ~ 2025-11-10)
+INSERT INTO EXTRACURRICULAR_SCHEDULE (EDU_MNG_ID, EDU_DT, EDU_EDN_TM) VALUES
+    (11, '2025-11-10 09:00:00', '17:00:00');
+
+-- 12. 성과 향상 워크숍 B (2025-11-15 ~ 2025-11-17)
+INSERT INTO EXTRACURRICULAR_SCHEDULE (EDU_MNG_ID, EDU_DT, EDU_EDN_TM) VALUES
+                                                                          (12, '2025-11-15 09:00:00', '17:00:00'),
+                                                                          (12, '2025-11-17 09:00:00', '17:00:00');
+
+-- 13. 학사 제도 안내 세미나 A (2025-11-20 ~ 2025-11-20)
+INSERT INTO EXTRACURRICULAR_SCHEDULE (EDU_MNG_ID, EDU_DT, EDU_EDN_TM) VALUES
+    (13, '2025-11-20 09:00:00', '17:00:00');
+
+-- 14. 학사 정책 설명회 B (2025-11-25 ~ 2025-11-26)
+INSERT INTO EXTRACURRICULAR_SCHEDULE (EDU_MNG_ID, EDU_DT, EDU_EDN_TM) VALUES
+                                                                          (14, '2025-11-25 09:00:00', '17:00:00'),
+                                                                          (14, '2025-11-26 09:00:00', '17:00:00');

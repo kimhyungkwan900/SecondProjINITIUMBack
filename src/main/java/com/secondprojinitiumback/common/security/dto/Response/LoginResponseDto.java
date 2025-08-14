675713f82e71dae24ad0ffe152699b9a601d8c03
김형관 <bearson0115@gmail.com>
2025-08-14 18:09:39 +0900

    2025_08_14_1809_김형관

--- src/main/java/com/secondprojinitiumback/common/security/dto/Response/LoginResponseDto.java
+++ src/main/java/com/secondprojinitiumback/common/security/dto/Response/LoginResponseDto.java
@@ -1,6 +1,5 @@
 package com.secondprojinitiumback.common.security.dto.Response;
 
-import com.secondprojinitiumback.common.security.dto.Response.UserDetailDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
